c04a76118edf52282d4a2ba6d8139392708e86c1
anch0vy <anch0vy.korea@gmail.com>
2016-01-19 08:36:53 +0900

    임포트 추가, PEP8에 맞게수정

--- checkDelete.py
+++ checkDelete.py
@@ -1,13 +1,18 @@
-def checkDelete(html) :
-	breakList = getArticleNumbersFromList(html)
-	# 페이지의 글 넘버 리스트를 반환 받음
-	first = breakList[0]
-	last = breakList[-1]
-	# 각각의 첫번째, 마지막 넘버
-	for notice in range(first, last) :
-		if session.query(Article).filter(Article.id == notice).count() is not 0 :
-			# Article.id 에 같은 넘버가 있으면 삭제 안된 것
-			continue
-		else :
-			session.update(Article).where(Article.id == notice).value(isDelete = 1)
-			# 없으면 삭제되었으니 isDelete 값 변경
+from db import Article, Session
+from crawl_page import getArticleNumbersFromList
+
+
+def checkDelete(html):
+    session = Session()
+    breakList = getArticleNumbersFromList(html)
+    # 페이지의 글 넘버 리스트를 반환 받음
+    first = breakList[0]
+    last = breakList[-1]
+    # 각각의 첫번째, 마지막 넘버
+    for notice in range(first, last):
+        if session.query(Article).filter(Article.id == notice).count() is not 0:
+            # Article.id 에 같은 넘버가 있으면 삭제 안된 것
+            continue
+        else:
+            session.update(Article).where(Article.id == notice).value(isDelete=1)
+            # 없으면 삭제되었으니 isDelete 값 변경
