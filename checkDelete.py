12fb9dd20833ea9acd0dcad157c8d09f7219d5d7
anch0vy <anch0vy.korea@gmail.com>
2016-01-20 02:49:45 +0900

    checkdelete 이상하게 머지된거 수정

--- checkDelete.py
+++ checkDelete.py
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-from db import Article, Session
-from crawl_page import getArticleNumbersFromList
-
-
-def checkDelete(html):
-    session = Session()
-    breakList = getArticleNumbersFromList(html)
-    # 페이지의 글 넘버 리스트를 반환 받음
-    first = breakList[0]
-    last = breakList[-1]
-    # 각각의 첫번째, 마지막 넘버
-    for notice in range(first, last):
-        if session.query(Article).filter(Article.id == notice).count() is not 0:
-            # Article.id 에 같은 넘버가 있으면 삭제 안된 것
-            continue
-        else:
-            session.update(Article).where(Article.id == notice).value(isDelete=1)
-            # 없으면 삭제되었으니 isDelete 값 변경
-    session.commit()
-=======
 # -*- coding: utf-8 -*-
 from crawl_page import getArticleNumbersFromList
 from db import Session, Article
@@ -52,4 +31,3 @@ if __name__ == '__main__':
         exit(1)
     opn = urllib2.urlopen("http://gall.dcinside.com/board/lists/?id={}".format(g))
     checkDelete(opn.read())
->>>>>>> refs/remotes/origin/checkDelete
